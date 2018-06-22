085b5f981fcbf36fa01b78915a6f1eddbe9b79d1
tangopapa <tporter@dtool.com>
2018-06-22 17:28:51 -0400

    added working nmap to nikto

--- dockter-tom/docker-entrypoint.sh
+++ dockter-tom/docker-entrypoint.sh
@@ -14,6 +14,10 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { "$@" || die "cannot $*"; }
 
+# $0 is a script name, 
+# $1 is IP address
+# $2 $3, $4, $5 are our command line arguments
+CMD=$1
 
 # Trap CTRL+C, CTRL+Z and quit 
 # trap '' SIGINT SIGQUIT SIGTSTP
@@ -22,12 +26,11 @@ try() { "$@" || die "cannot $*"; }
 
 DX="docker exec -it"
 DXE="docker exec -it -e"
-PROG1="/usr/local/bin/nmap localhost"
+PROG1="/usr/local/bin/nmap -p80, 443, 3306 localhost -oG - tee 'perl nikto -h' /opt/sqlmap/sqlmap.py localhost "
 PROG2="brakeman -q </path/to/application> -o output.json -o output"
 PROG3="perl nikto -h localhost -p 80,88,443"
 PROG4="/opt/sqlmap/sqlmap.py $1"
 PROG5="/opt/debcheck/dependency-checker.sh <path/to/file or directory>"
-
 
 
 ## Functions
@@ -37,12 +40,24 @@ function nikto2 { $PROG3 }
 #function sqlmap { true; }
 #function OWASPdc { true; }
 
+case "$CMD" in
+  "nmap" )
+    exec nmap
+    ;;
 
-nmap
-nikto2
+  "nikto2" )
+    # we can modify files here, using ENV variables passed in 
+    # "docker create" command. It can't be done during build process.
+    exec nikto2
+    ;;
+
+   * )
+    # Run custom command. Thanks to this line we can still use 
+    # "docker run our_image /bin/bash" and it will work
+    exec $CMD ${@:2}
+    ;;
+esac
 
 
-## Do everything in this .sh script, then in the same shell run the command the user passes in on the command line"
-/bin/bash
 
 
